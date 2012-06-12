2ba1d648e62ead70657ef3007dd9a7cb294fb95c
Maciej Borzecki <maciej@blinkbox.com>
2012-06-12 15:05:50 +0100

    BB addjob page:
    - introducing two column layout
    - hiding browser description if browser group is hidden

--- inc/pages/AddjobBBPage.php
+++ inc/pages/AddjobBBPage.php
@@ -21,7 +21,7 @@ class AddjobbbPage extends Page {
 		$request = $this->getContext()->getRequest();
 
 		$this->setTitle( "Add new job BB" );
-		$this->bodyScripts[] = swarmpath( "js/addjob.js" );
+		$this->bodyScripts[] = swarmpath( "js/addjobBB.js" );
 
 		$html = "";
 
@@ -76,91 +76,61 @@ class AddjobbbPage extends Page {
 		
 		$formHtml = <<<HTML
 <form action="$addjobPageUrl" method="post" class="form-horizontal">
-
-	<fieldset>
-		<legend>Authentication</legend>
-
-		<div class="control-group">
-			<label class="control-label" for="form-authUsername">User name:</label>
-			<div class="controls">
-				<input type="text" name="authUsername" value="$userName" id="form-authUsername">
-			</div>
-		</div>
-		<div class="control-group">
-			<label class="control-label" for="form-authToken">Auth token:</label>
-			<div class="controls">
-				<input type="text" name="authToken" id="form-authToken" class="input-xlarge" value="$authToken">
-			</div>
-		</div>
-	</fieldset>
-
-	<fieldset>
-		<legend>Job information</legend>
-
-		<div class="control-group">
-			<label class="control-label" for="form-jobName">Job name:</label>
-			<div class="controls">
-				<input type="text" name="jobName" id="form-jobName" class="input-xlarge" maxlength="255" value="$jobName">
-				<span class="help-inline">HTML, up to 255 characters</span>
-			</div>
-		</div>
-		<div class="control-group">
-			<label class="control-label" for="form-runMax">Run max:</label>
-			<div class="controls">
-				<input type="number" size="5" name="runMax" id="form-runMax" value="$runMax" min="1" max="99">
-				<p class="help-block">This is the maximum number of times a run is ran in a user agent. If a run passes
-				without failures then it is only ran once. If it does not pass, TestSwarm will re-try the run
-				(up to "Run max" times) for that useragent to avoid error pollution due to time-outs, slow
-				computers or other unrelated conditions that can cause the server to not receive a success report.</p>
-			</div>
-		</div>
-	</fieldset>
-
-	<fieldset>
-		<legend>Browsers</legend>
-
-		<p>Choose which groups of user agents this job should be ran in. Some of the groups may
-		overlap each other, TestSwarm will detect and remove duplicate entries in the resulting set.</p>
-
+	<div class="row">
+		<div class="span6">
 HTML;
-		foreach ( $conf->browserSets as $set => $browsers ) {
-			$set = htmlspecialchars( $set );
-			$browsersHtml = '';
-			$last = count( $browsers ) - 1;
-			foreach ( $browsers as $i => $browser ) {
-				if ( $i !== 0 ) {
-					$browsersHtml .= $i === $last ? '<br> and ' : ',<br>';
-				} else {
-					$browsersHtml .= '<br>';
-				}
-				$browsersHtml .= htmlspecialchars( $swarmUaIndex->$browser->displaytitle );
-			}		
-			$checked = "";
-			foreach ( $selectedBrowsers as $b ) {
-					if($b == $set) {
-					$checked = "checked=\"checked\"";
-					break;
-				}
-			}			
-			$formHtml .= <<<HTML
-		<div class="control-group">
-			<label class="checkbox" for="form-browserset-$set">
-				<input type="checkbox" name="browserSets[]" value="$set" id="form-browserset-$set" $checked>
-				<strong>$set</strong>: $browsersHtml.
-			</label>
-		</div>
-HTML;
-		}
-
+		$formHtml .= $this->getBrowsers($conf, $swarmUaIndex, $selectedBrowsers);
 		$formHtml .= <<<HTML
-	</fieldset>
+		</div>
+		<div class="span6">
+		
+			<fieldset>
+				<legend>Authentication</legend>
 
-	<fieldset>
-		<legend>Runs</legend>
+				<div class="control-group">
+					<label class="control-label" for="form-authUsername">User name:</label>
+					<div class="controls">
+						<input type="text" name="authUsername" value="$userName" id="form-authUsername">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="form-authToken">Auth token:</label>
+					<div class="controls">
+						<input type="text" name="authToken" id="form-authToken" class="input-xlarge" value="$authToken">
+					</div>
+				</div>
+			</fieldset>
+			
+			<fieldset>
+				<legend>Job information</legend>
 
-		<p>Each job consists of several runs. Every run has a name and a url to where that test suite can be ran. All the test suites should probably have the same common code base or some other grouping characteristic, where each run is part of the larger test suite. As example, for a QUnit test suite the <code>filter</code> url parameter can be used to only run one of the "modules" so every run would be the name of that module and the URL to the testsuite with <code>?filter=modulename</code> appended to it.</p>
+				<div class="control-group">
+					<label class="control-label" for="form-jobName">Job name:</label>
+					<div class="controls">
+						<input type="text" name="jobName" id="form-jobName" class="input-xlarge" maxlength="255" value="$jobName">
+						<span class="help-inline">HTML, up to 255 characters</span>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="form-runMax">Run max:</label>
+					<div class="controls">
+						<input type="number" size="5" name="runMax" id="form-runMax" value="$runMax" min="1" max="99">
+						<p class="help-block">This is the maximum number of times a run is ran in a user agent. If a run passes
+						without failures then it is only ran once. If it does not pass, TestSwarm will re-try the run
+						(up to "Run max" times) for that useragent to avoid error pollution due to time-outs, slow
+						computers or other unrelated conditions that can cause the server to not receive a success report.</p>
+					</div>
+				</div>
+			</fieldset>
+HTML;
+		$formHtml .= <<<HTML
 
-		<div id="runs-container" class="well">
+			<fieldset>
+				<legend>Runs</legend>
+
+				<p>Each job consists of several runs. Every run has a name and a url to where that test suite can be ran. All the test suites should probably have the same common code base or some other grouping characteristic, where each run is part of the larger test suite. As example, for a QUnit test suite the <code>filter</code> url parameter can be used to only run one of the "modules" so every run would be the name of that module and the URL to the testsuite with <code>?filter=modulename</code> appended to it.</p>
+
+				<div id="runs-container" class="well">
 HTML;
 		if($runNames && count($runNames)>0) {
 			for($i=0;$i<count($runNames);$i++) {
@@ -174,9 +144,12 @@ HTML;
 		}
 
 		$formHtml .= <<<HTML
+				</div>
+			</fieldset>
+			
 		</div>
-	</fieldset>
-
+	</div>
+	
 	<div class="form-actions">
 		<input type="submit" value="Create job" class="btn btn-primary btn-large">
 	</div>
@@ -196,7 +169,50 @@ HTML;
 					<br>
 					<label for="form-runUrls1">Run URL:</label>
 					<input type="text" name="runUrls[]" placeholder="http://" class="input-xlarge" id="form-runUrls$i" value="$url">
-				</fieldset>
+				</fieldset>				
 HTML;
+	}
+	
+	private function getBrowsers($conf, $swarmUaIndex, $selectedBrowsers) {
+		$formHtml = <<<HTML
+			<fieldset>
+				<legend>Browsers</legend>
+
+				<p>Choose which groups of user agents this job should be ran in. Some of the groups may
+				overlap each other, TestSwarm will detect and remove duplicate entries in the resulting set.</p>
+HTML;
+		foreach ( $conf->browserSets as $set => $browsers ) {
+			$set = htmlspecialchars( $set );
+			$browsersHtml = '';
+			$last = count( $browsers ) - 1;
+			foreach ( $browsers as $i => $browser ) {
+				if ( $i !== 0 ) {
+					$browsersHtml .= $i === $last ? '<br> and ' : ',<br>';
+				}
+				$browsersHtml .= htmlspecialchars( $swarmUaIndex->$browser->displaytitle );
+			}		
+			$checked = "";
+			$hideBrowserDetails = "";
+			foreach ( $selectedBrowsers as $b ) {
+				if($b == $set) {
+					$checked = "checked=\"checked\"";
+					break;
+				} else {
+					$hideBrowserDetails = "hide";
+				}
+			}			
+			$formHtml .= <<<HTML
+			<div class="control-group">
+				<label class="checkbox" for="form-browserset-$set">
+					<input type="checkbox" name="browserSets[]" value="$set" id="form-browserset-$set" $checked>
+					<strong>$set</strong><div class="browser-details $hideBrowserDetails">$browsersHtml</div>
+				</label>
+			</div>
+HTML;
+		}
+		$formHtml .= <<<HTML
+			</fieldset>
+HTML;
+		return $formHtml;	
 	}
 }
