a809a89e9aedba6b561f8f1bcc43f6e24595495d
Maciej Borzecki <maciej@blinkbox.com>
2012-06-12 12:41:45 +0100

    extracting run url and name from querystring on bb addjob page

--- inc/pages/AddjobBBPage.php
+++ inc/pages/AddjobBBPage.php
@@ -20,7 +20,7 @@ class AddjobbbPage extends Page {
 	protected function initContent() {
 		$request = $this->getContext()->getRequest();
 
-		$this->setTitle( "Add new job" );
+		$this->setTitle( "Add new job BB" );
 		$this->bodyScripts[] = swarmpath( "js/addjob.js" );
 
 		$html = "";
@@ -46,7 +46,7 @@ class AddjobbbPage extends Page {
 
 		return $html;
 	}
-
+	
 	protected function getAddjobFormHtml() {
 		$conf = $this->getContext()->getConf();
 		$request = $this->getContext()->getRequest();
@@ -56,6 +56,23 @@ class AddjobbbPage extends Page {
 		$addjobPageUrl = htmlspecialchars( swarmpath( "addjob" ) );
 		$userName = $request->getSessionData( "username" ) && $request->getSessionData( "auth" ) == "yes"  ? htmlspecialchars( $request->getSessionData( "username" ) ) : "";
 
+		// fields to be taken from the querystring:
+		$authUsername = $request->getVal('authUsername'); 
+		$authToken = $request->getVal('authToken'); 
+		$jobName = $request->getVal('jobName');
+		$runMax = $request->getVal('runMax', 3);
+		$runNames = $request->getArray('runNames');
+		$runUrls = $request->getArray('runUrls');	
+		
+		if($authUsername) {
+			$userName = $authUsername;
+		}
+		
+		if ( $runNames && !$runUrls || !$runNames && $runUrls || $runNames && $runUrls && count($runNames) != count($runUrls) ) {
+			throw new SwarmException( "runNames and runUrls must have the same number of elements in the array" );
+			return;
+		}
+		
 		$formHtml = <<<HTML
 <form action="$addjobPageUrl" method="post" class="form-horizontal">
 
@@ -71,7 +88,7 @@ class AddjobbbPage extends Page {
 		<div class="control-group">
 			<label class="control-label" for="form-authToken">Auth token:</label>
 			<div class="controls">
-				<input type="text" name="authToken" id="form-authToken" class="input-xlarge">
+				<input type="text" name="authToken" id="form-authToken" class="input-xlarge" value="$authToken">
 			</div>
 		</div>
 	</fieldset>
@@ -82,14 +99,14 @@ class AddjobbbPage extends Page {
 		<div class="control-group">
 			<label class="control-label" for="form-jobName">Job name:</label>
 			<div class="controls">
-				<input type="text" name="jobName" id="form-jobName" class="input-xlarge" maxlength="255">
+				<input type="text" name="jobName" id="form-jobName" class="input-xlarge" maxlength="255" value="$jobName">
 				<span class="help-inline">HTML, up to 255 characters</span>
 			</div>
 		</div>
 		<div class="control-group">
 			<label class="control-label" for="form-runMax">Run max:</label>
 			<div class="controls">
-				<input type="number" size="5" name="runMax" id="form-runMax" value="3" min="1" max="99">
+				<input type="number" size="5" name="runMax" id="form-runMax" value="$runMax" min="1" max="99">
 				<p class="help-block">This is the maximum number of times a run is ran in a user agent. If a run passes
 				without failures then it is only ran once. If it does not pass, TestSwarm will re-try the run
 				(up to "Run max" times) for that useragent to avoid error pollution due to time-outs, slow
@@ -136,33 +153,19 @@ HTML;
 		<p>Each job consists of several runs. Every run has a name and a url to where that test suite can be ran. All the test suites should probably have the same common code base or some other grouping characteristic, where each run is part of the larger test suite. As example, for a QUnit test suite the <code>filter</code> url parameter can be used to only run one of the "modules" so every run would be the name of that module and the URL to the testsuite with <code>?filter=modulename</code> appended to it.</p>
 
 		<div id="runs-container" class="well">
-			<fieldset>
-				<legend>Run 1</legend>
+HTML;
+		if($runNames && count($runNames)>0) {
+			for($i=0;$i<count($runNames);$i++) {
+				$formHtml .= $this->getRun($i+1, $runNames[$i], $runUrls[$i]);
+			}		
+		}		
+		else {		
+			for($i=0;$i<3;$i++) {
+				$formHtml .= $this->getRun($i+1, $runNames[$i], $runUrls[$i]);
+			}
+		}
 
-				<label for="form-runNames1">Run name:</label>
-				<input type="text" name="runNames[]" id="form-runNames1" maxlength="255">
-				<br>
-				<label for="form-runUrls1">Run URL:</label>
-				<input type="text" name="runUrls[]" placeholder="http://" class="input-xlarge"id="form-runUrls1">
-			</fieldset>
-			<fieldset>
-				<legend>Run 2</legend>
-
-				<label for="form-runNames2">Run name:</label>
-				<input type="text" name="runNames[]" id="form-runNames2" maxlength="255">
-				<br>
-				<label for="form-runUrls2">Run URL:</label>
-				<input type="text" name="runUrls[]" placeholder="http://" class="input-xlarge"id="form-runUrls2">
-			</fieldset>
-			<fieldset>
-				<legend>Run 3</legend>
-
-				<label for="form-runNames3">Run name:</label>
-				<input type="text" name="runNames[]" id="form-runNames3" maxlength="255">
-				<br>
-				<label for="form-runUrls3">Run URL:</label>
-				<input type="text" name="runUrls[]" placeholder="http://" class="input-xlarge"id="form-runUrls3">
-			</fieldset>
+		$formHtml .= <<<HTML
 		</div>
 	</fieldset>
 
@@ -173,5 +176,19 @@ HTML;
 HTML;
 
 		return $formHtml;
+	}
+	
+	private function getRun($i, $name = null, $url = null) {
+		return <<<HTML
+				<fieldset>
+					<legend>Run $i</legend>
+
+					<label for="form-runNames1">Run name:</label>
+					<input type="text" name="runNames[]" id="form-runNames$i" maxlength="255" value="$name">
+					<br>
+					<label for="form-runUrls1">Run URL:</label>
+					<input type="text" name="runUrls[]" placeholder="http://" class="input-xlarge" id="form-runUrls$i" value="$url">
+				</fieldset>
+HTML;
 	}
 }
